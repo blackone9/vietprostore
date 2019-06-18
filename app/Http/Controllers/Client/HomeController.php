b2f953629701361fe18fdca0706093cee615aefe
haitran <haituit9x@gmail.com>
2019-06-18 20:14:45 +0700

    implements client views

--- app/Http/Controllers/Client/HomeController.php
+++ app/Http/Controllers/Client/HomeController.php
@@ -7,5 +7,16 @@ use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('client.index');
+    }
+    public function about()
+    {
+        return view('client.about');
+    }
+    public function contact()
+    {
+        return view('client.contact');
+    }
 }
--- app/Http/Controllers/Client/ProductController.php
+++ app/Http/Controllers/Client/ProductController.php
@@ -7,5 +7,12 @@ use App\Http\Controllers\Controller;
 
 class ProductController extends Controller
 {
-    //
+    public function detail()
+    {
+        return view('client.product');
+    }
+    public function shop()
+    {
+        return view('client.shop');
+    }
 }
